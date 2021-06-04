131acea363929b93529b4d34580533168d5cc9c4
alvynah <alvynahw@gmail.com>
2021-06-04 08:12:41 +0300

    Add post,put and delete requests for neighbors

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -5,4 +5,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('api/neighbors/', views.NeighborhoodList.as_view(), name='neighbor'),
+    path('api/neighbors/update/<int:pk>/', views.NeighborhoodList.as_view(), name='neighbor_update'),
+    path('api/neighbors/delete/<int:pk>/', views.NeighborhoodList.as_view(), name='neighbor_delete'),
 ]
--- neighbor/views.py
+++ neighbor/views.py
@@ -8,6 +8,11 @@ from .serializers import *
 # Create your views here.
 
 class  NeighborhoodList(APIView):
+    def get_neighborhood(self, pk):
+        try:
+            return Neighborhood.objects.get(pk=pk)
+        except Neighborhood.DoesNotExist:
+            return Http404 
     def get(self,request,format=None):
        neighborhood=Neighborhood.objects.all()
        serializers=NeighborhoodSerializer(neighborhood,many=True)
@@ -27,3 +32,24 @@ class  NeighborhoodList(APIView):
             }
             return Response(response,status=status.HTTP_200_OK)
         return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+    def put(self, request, pk, format=None):
+        neighborhood = self.get_neighborhood(pk)
+        serializers = NeighborhoodSerializer(neighborhood, request.data)
+        if serializers.is_valid():
+            serializers.save()
+            neighborhood=serializers.data
+            response = {
+                     'data': {
+                     'neighborhood': dict(neighborhood),
+                     'status': 'success',
+                    'message': 'neighborhood updated successfully',
+                 }
+             }
+            return Response(response)
+        else:
+            return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        neighborhood = self.get_neighborhood(pk)
+        neighborhood.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
