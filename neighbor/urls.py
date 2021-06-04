03a645039295ec8187d5bbbc96857b985592eba5
alvynah <alvynahw@gmail.com>
2021-06-04 06:46:27 +0300

    Add views for get neighborhood successfully

--- neighbor/urls.py
+++ neighbor/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, re_path
 from django.conf import settings
 from . import views
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('welcome',views.welcome,name='welcome'),
+    path('api/neighbors/', views.NeighborhoodList.as_view(), name='neighbor'),
 ]
--- neighbor/views.py
+++ neighbor/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render,HttpResponse
-
+from django.http import Http404
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import *
+from .serializers import *
 # Create your views here.
 
-def welcome(request):
-   return HttpResponse("Wow")
+class  NeighborhoodList(APIView):
+    def get(self,request,format=None):
+       neighborhood=Neighborhood.objects.all()
+       serializers=NeighborhoodSerializer(neighborhood,many=True)
+       return Response(serializers.data) 
